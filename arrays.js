3c8b97f25e73d31c4753738312e809bbe13cfb33
Dustin Doyle <debodoyle@gmail.com>
2018-01-25 15:21:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,10 @@ var chocolateBars = ['snickers', 'hundred grand','kitkat', 'skittles'];
 // .push appends on element onto the end of the Array
 // .unshift takes an element of the beginning
 
+function addElementToBeginningOfArray() {
+  
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+  
+}
