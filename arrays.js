bc2adfab5595e76b90fc41e754e30d8d6213a652
Dustin Doyle <debodoyle@gmail.com>
2018-01-25 16:48:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,5 +40,8 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
   return chocolateBars;
 }
 
-
+function removeElementFromEndOfArray(chocolateBars) {
+  chocolateBars.slice(0, chocolateBars.length - 1);
+  return chocolateBars;
+}
 
