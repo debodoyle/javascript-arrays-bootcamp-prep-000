9ce8e82f13a7cba33d953f0cd70a42bdbf24cc2d
Dustin Doyle <debodoyle@gmail.com>
2018-01-25 15:31:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ var chocolateBars = ['snickers', 'hundred grand','kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array,element) {
   array = "foo";
-  
+  element = 1;
 }
 
 function destructivelyAddElementToBeginningOfArray() {
