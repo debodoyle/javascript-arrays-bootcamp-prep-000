1680379655820b7ce0caf9e6c264fed5e404e435
Dustin Doyle <debodoyle@gmail.com>
2018-01-25 16:05:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,6 +26,7 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
-  return chocolateBars.shift;
+  chocolateBars.unshift(chocolateBars);
+  return chocolateBars;
 }
 
